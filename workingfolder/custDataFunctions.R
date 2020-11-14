474633cf5d6c484066e347eb9c3b7c620ed96aa1
kokada60 <kokada60@gmail.com>
2020-11-14 14:48:25 -0800

    Commit Chapter 5_2020_11_14

--- workingfolder/custDataFunctions.R
+++ workingfolder/custDataFunctions.R
@@ -1,3 +1,10 @@
 count_missings <- function(dr) {
   sapply(dr, FUN=function(col) { sum(is.na(col))})
 }
+
+## The function returns 0 for any number abs(x) <= 1. The log-10 of such x will be a negative #. 
+##  log10 of such x will be a negative number, overwrapping with the artificially negated signed x, 
+##  that's a negative number to begin with. 
+signedLog10 <- function(x) {  
+  ifelse(abs(x) <= 1, 0, sign(x) * log10(abs(x)))
+}
